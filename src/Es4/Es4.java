b84136e052d50ed2c4a1fc960f418b5eabe4ab14
Carmen Caniglia <carmencaniglia@gmail.com>
2023-11-28 15:00:00 +0100

    fourth Es

---
+++ src/Es4/Es4.java
@@ -0,0 +1,17 @@
+package Es4;
+
+import java.util.Scanner;
+
+public class Es4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Inserisci un numero");
+        n = input.nextInt();
+
+        for (int i = n; i >= 0; i--){
+            System.out.println(i);
+        }
+        input.close();
+    }
+}
