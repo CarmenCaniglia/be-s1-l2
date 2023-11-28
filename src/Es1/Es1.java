ea726810ddf80dd1b7a2578abf3d007588d80625
Carmen Caniglia <carmencaniglia@gmail.com>
2023-11-28 14:06:25 +0100

    first Es

--- src/Es1/Es1.java
+++ src/Es1/Es1.java
@@ -1,7 +1,31 @@
 package Es1;
 
 public class Es1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //Punto 1
+    public static boolean strEvenOdd (String str) {
+        return str.length() % 2 == 0;
+    }
+
+    //Punto 2
+    public static boolean leapYear (int year){
+        if(year % 4 == 0) {
+            if (year % 100 == 0){
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "frase a caso";
+        System.out.println("il numero di caratteri di " + str + " è " + (strEvenOdd(str) ? "pari" : "dispari"));
+
+        int year = 2023;
+        int nextYear= 2024;
+        System.out.println("L'anno " + year + " è bisestile?" + leapYear(year));
+        System.out.println("L'anno " + nextYear + " è bisestile?" + leapYear(nextYear));
     }
 }
